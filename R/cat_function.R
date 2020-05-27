84055f70167da1bcb2b3a4e1fada63e2f4b6be7a
Manu Chevalier <chevalier.manuel@gmail.com>
2020-05-27 09:56:35 +0200

    docu cats

--- R/cat_function.R
+++ R/cat_function.R
@@ -1,4 +1,13 @@
-cat_function <- function(love=TRUE){
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+
+ cat_function <- function(love=TRUE){
     if(love==TRUE){
         print("I love cats!")
     }
